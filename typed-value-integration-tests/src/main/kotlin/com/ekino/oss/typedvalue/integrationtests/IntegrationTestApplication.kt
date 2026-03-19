5af4bd2b0149a9dc115c51264f2d92c32f81ae46
Benoît Havret <benoit.havret@ekino.com>
2026-03-19 11:51:54 +0100

    chore: enable detekt for integration-tests module
    Use a Gradle resolution strategy to pin detekt's Kotlin version,
    preventing Spring Dependency Management from overriding it.
    Also add UnnecessaryAbstractClass excludes for hibernate module.

--- typed-value-integration-tests/src/main/kotlin/com/ekino/oss/typedvalue/integrationtests/IntegrationTestApplication.kt
+++ typed-value-integration-tests/src/main/kotlin/com/ekino/oss/typedvalue/integrationtests/IntegrationTestApplication.kt
@@ -8,6 +8,7 @@ import org.springframework.boot.runApplication
 
 @SpringBootApplication class IntegrationTestApplication
 
+@Suppress("SpreadOperator")
 fun main(args: Array<String>) {
   runApplication<IntegrationTestApplication>(*args)
 }
